3972909be6ccf063ec6e16af1e874071b2ad34e1
Ngwang shalom <94115051+Ngwangshalom@users.noreply.github.com>
2026-06-01 20:24:28 +0100

    Refactor code structure for improved readability and maintainability

---
+++ android/app/.cxx/Debug/174e1fa1/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "/root/.gradle/caches/8.13/transforms/0b17c7b235c8f3b3b83e3834fa35af77/transformed/jetified-hermes-android-0.82.1-debug/prefab/modules/hermesvm/libs/android.armeabi-v7a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/root/.gradle/caches/8.13/transforms/0b17c7b235c8f3b3b83e3834fa35af77/transformed/jetified-hermes-android-0.82.1-debug/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
